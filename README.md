c63b4777513c28c9a49a7a52a27cc08e68b56833
chenyingchao <chenyingchao@xigua.city>
2020-11-11 23:36:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#### 运行和生成报告
+mvn clean test  
+mvn io.qameta.allure:allure-maven:serve 打开报告     
+mvn io.qameta.allure:allure-maven:report 生成报告(site)  
---
+++ src/test/java/com/cn/DemoTest.java
@@ -0,0 +1,10 @@
+package com.cn;
+
+import org.testng.annotations.*;
+public class DemoTest {
+
+    @Test
+    void test(){
+        System.out.println("AAAAAAAA");
+    }
+}
